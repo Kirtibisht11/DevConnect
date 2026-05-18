e66fcfd072ef789b979dd2beea1181548e5be4a0
Kirtibisht11 <kirtibisht2908@gmail.com>
2026-05-18 12:46:26 +0530

    fix: update CORS for production frontend

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin:  [
+    "http://localhost:5173",
+    "https://dev-connect-8awfnm9dh-kirtis-projects-084efe1d.vercel.app"
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
