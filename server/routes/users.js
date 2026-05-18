ec02b59139f62c84c69a2c842b5f57a8b51a0b62
Kirtibisht11 <kirtibisht2908@gmail.com>
2026-05-18 12:34:26 +0530

    fix: remove duplicate imports and fix route conflict

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { getUserById, updateProfile } = require('../controllers/userController');
+
+const { getUserById, updateProfile, getSuggestions } = require('../controllers/userController');
 const { toggleFollow, getFollowers, getFollowing } = require('../controllers/followController');
 const { protect } = require('../middleware/auth');
-const { getUserById, updateProfile, getSuggestions } = require('../controllers/userController');
+router.get('/suggestions', protect, getSuggestions);
+
 router.get('/:id', getUserById);
 router.put('/:id', protect, updateProfile);
+
 router.post('/:id/follow', protect, toggleFollow);
 router.get('/:id/followers', getFollowers);
 router.get('/:id/following', getFollowing);
-router.get('/suggestions', protect, getSuggestions);
+
 
 module.exports = router;
